2a28ef889a60d8d988dc0c69f29ca6e44248d305
Vinay jain <jainv823@gmail.com>
2023-05-12 15:32:48 +0530

    Created Basic App Skeleton

--- App.tsx
+++ App.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from './src/screen/HomeScreen';
+import DetailsScreen from './src/screen/DetailsScreen';
 
+export type RootStackParamList = {
+  Home: undefined;
+  Details: {product: Product};
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 function App(): JSX.Element {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>SHOPPING APP</Text>
-      </View>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Trending Products'}}
+        />
+        <Stack.Screen
+          name="Details"
+          component={DetailsScreen}
+          options={{title: 'Product Details'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ src/index.d.ts
@@ -0,0 +1,10 @@
+interface Product {
+  id: string;
+  name: string;
+  imageUrl: string;
+  orignalPrice: number;
+  discountPrice: number;
+  offerPercentage: number;
+  rating: number;
+  ratingCount: number;
+}
